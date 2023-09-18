c51292eaf8511f600e191eee96e6e3e1eecf8bc4
Paul Botros <pbotros@ucsd.edu>
2023-09-18 15:30:51 -0700

    Exposing an HTTP server for the ingester
    Allows dynamic querying of streams as well as direct retrieval of
    metadata and data contents.

---
+++ cpp/ingester/src/ingester_http_server.cpp
@@ -0,0 +1,176 @@
+//
+// Created by Paul Botros on 9/18/23.
+//
+
+#include "ingester_http_server.h"
+#include <set>
+#include <optional>
+#include <filesystem>
+#include <utility>
+#include <glog/logging.h>
+
+using json = nlohmann::json;
+
+namespace river {
+
+IngesterHttpServer::IngesterHttpServer(std::filesystem::path root_directory, int port) :
+    root_directory_(std::move(root_directory)), port_(port) {}
+
+void IngesterHttpServer::Start() {
+    svr_ = std::make_unique<httplib::Server>();
+    svr_running_thread_ = std::make_unique<std::thread>(&IngesterHttpServer::RunServerForever, this);
+}
+
+void IngesterHttpServer::Stop() {
+    if (svr_) {
+        svr_->stop();
+    }
+    if (svr_running_thread_) {
+        if (svr_running_thread_->joinable()) {
+            svr_running_thread_->join();
+        }
+        svr_running_thread_.reset();
+    }
+    svr_.reset();
+}
+
+void IngesterHttpServer::RunServerForever() {
+    svr_->Get("/api/streams", [this](const httplib::Request &req, httplib::Response &res) {
+        this->HandleStreamsGet(req, res);
+    });
+    svr_->Get(R"(/api/streams/([^/]+))", [this](const httplib::Request &req, httplib::Response &res) {
+        auto stream_name = req.matches[1];
+        this->HandleStreamGet(req, res, stream_name);
+    });
+    svr_->Get(R"(/api/streams/([^/]+)/data.parquet)", [this](const httplib::Request &req, httplib::Response &res) {
+        auto stream_name = req.matches[1];
+        this->HandleStreamGetParquet(req, res, stream_name);
+    });
+
+    svr_->listen("0.0.0.0", port_);
+}
+
+void IngesterHttpServer::HandleStreamsGet(const httplib::Request &request, httplib::Response &response) {
+    //--- filenames are unique so we can use a set
+    std::set<std::filesystem::path> sorted_by_name;
+
+    for (auto &entry : std::filesystem::directory_iterator(root_directory_)) {
+        sorted_by_name.insert(entry.path());
+    }
+
+    json streams_json;
+    for (const auto &stream_name : sorted_by_name) {
+        auto stream_json = GetStreamJson(stream_name);
+        if (stream_json) {
+            streams_json.push_back(stream_json.value());
+        }
+    }
+
+    json response_json;
+    response_json["streams"] = streams_json;
+    response.set_content(response_json.dump(), "application/json");
+}
+
+void IngesterHttpServer::HandleStreamGet(const httplib::Request &request,
+                                         httplib::Response &response,
+                                         const std::string &stream_name) {
+    json response_json;
+    auto val = GetStreamJson(stream_name);
+    if (!val.has_value()) {
+        response.set_content("Invalid stream name", "text/plain");
+        response.status = 404;
+        return;
+    }
+
+    response_json["stream"] = val.value();
+    response.set_content(response_json.dump(), "application/json");
+}
+
+void IngesterHttpServer::HandleStreamGetParquet(const httplib::Request &request,
+                                                httplib::Response &response,
+                                                const std::string &stream_name) {
+    auto data_filename = root_directory_ / stream_name / "data.parquet";
+    if (!std::filesystem::exists(data_filename)) {
+        response.set_content("Could not find data file", "text/plain");
+        response.status = 404;
+        return;
+    }
+
+    std::streampos data_f_size;
+    {
+        std::ifstream data_f(data_filename, std::ios::binary);
+        data_f_size = data_f.tellg();
+        data_f.seekg(0, std::ios::end);
+        data_f_size = data_f.tellg() - data_f_size;
+        data_f.seekg(0);
+        data_f.close();
+    }
+
+    // Not too big to overload memory: 16 MB as an arbitrary setting.
+    const int max_read_data_size = 16 * 1024 * 1024;
+    auto *read_data_buffer = new std::vector<char>(max_read_data_size);
+    auto *num_bytes_remaining_ptr = new std::streamsize();
+    *num_bytes_remaining_ptr = data_f_size;
+    response.set_chunked_content_provider(
+        "application/octet-stream", // Content type
+        [data_filename, read_data_buffer, num_bytes_remaining_ptr](size_t offset, httplib::DataSink &sink)mutable {
+            std::ifstream data_f(data_filename, std::ios::binary);
+            // If it's ever bad for whatever reason, terminate the stream
+            if (!data_f) {
+                return false;
+            }
+
+            data_f.seekg(offset);
+            std::streamsize bytes_to_read = (std::min<std::streamsize>)(
+                read_data_buffer->size(), *num_bytes_remaining_ptr);
+            if (bytes_to_read == 0) {
+                // Shouldn't ever get here; the "done" condition should be handled below.
+                return false;
+            }
+
+            data_f.read(read_data_buffer->data(), bytes_to_read);
+            std::streamsize num_bytes_read_this_time = data_f.gcount();
+            (*num_bytes_remaining_ptr) -= num_bytes_read_this_time;
+
+            sink.write(read_data_buffer->data(), num_bytes_read_this_time);
+            if (*num_bytes_remaining_ptr == 0) {
+                // Done!
+                sink.done();
+            }
+
+            return true;
+        },
+        [read_data_buffer, num_bytes_remaining_ptr](bool success) {
+            delete read_data_buffer;
+            delete num_bytes_remaining_ptr;
+        });
+}
+
+std::optional<json> IngesterHttpServer::GetStreamJson(const std::string &stream_name) {
+    auto metadata_filename = root_directory_ / stream_name / "metadata.json";
+    if (!std::filesystem::exists(metadata_filename)) {
+        return std::nullopt;
+    }
+
+    json ret;
+
+    json metadata;
+    {
+        std::ifstream metadata_f(metadata_filename);
+        metadata = json::parse(metadata_f);
+    }
+
+    try {
+        ret["stream_name"] = metadata["stream_name"];
+        ret["ingestion_status"] = metadata["ingestion_status"];
+        ret["initialized_at_us"] = std::stoull((std::string) metadata["initialized_at_us"]);
+        ret["local_minus_server_clock_us"] = std::stoll((std::string) metadata["local_minus_server_clock_us"]);
+    } catch (json::exception &e) {
+        LOG(INFO) << "Failed to parse metadata for stream " << stream_name;
+        return std::nullopt;
+    }
+
+    return ret;
+}
+
+}
---
+++ cpp/ingester/src/ingester_http_server.h
@@ -0,0 +1,40 @@
+//
+// Created by Paul Botros on 9/18/23.
+//
+
+#ifndef RIVER_INGESTER_SRC_INGESTER_HTTP_SERVER_H_
+#define RIVER_INGESTER_SRC_INGESTER_HTTP_SERVER_H_
+
+#include <httplib.h>
+#include <memory>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+namespace river {
+
+class IngesterHttpServer {
+public:
+    IngesterHttpServer(std::filesystem::path root_directory, int port);
+
+    // Starts in background
+    void Start();
+
+    void Stop();
+private:
+    const std::filesystem::path root_directory_;
+    const int port_;
+
+    void RunServerForever();
+
+    std::unique_ptr<httplib::Server> svr_;
+    std::unique_ptr<std::thread> svr_running_thread_;
+    void HandleStreamsGet(const httplib::Request &request, httplib::Response &response);
+    void HandleStreamGet(const httplib::Request &request, httplib::Response &response, const std::string &stream_name);
+    void HandleStreamGetParquet(const httplib::Request &request, httplib::Response &response, const std::string &stream_name);
+    std::optional<json> GetStreamJson(const std::string &stream_name);
+};
+
+}
+
+#endif //RIVER_INGESTER_SRC_INGESTER_HTTP_SERVER_H_
